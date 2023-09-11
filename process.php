2cc5dc7eba5636626db48edef41ddbb8c1a114df
dawittamene <dawittamene559@gmail.com>
2023-09-11 23:35:06 +0300

    commit

--- process.php
+++ process.php
@@ -24,20 +24,8 @@ if ($conn){
 }
    
 
-?>
-
-<?php
-
-session_start();
-if(isset($_SESSION['view'])){
-    $_SESSION['view'] = $_SESSION['view'] + 1;
-
-}else{
-    $_SESSION ['view'] = 1;
-}
 
 ?>
-
 
 
 <!DOCTYPE html>
